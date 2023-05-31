be8ae38aa918208b9387fa23b8b1a49c2fc4a357
gill3371 <gilles.bailly@agencealchimie.fr>
2023-05-31 07:20:42 +0200

    header+footer

---
+++ wp-content/themes/planty_ok/functions.php
@@ -0,0 +1,37 @@
+<?php
+
+// Action qui permet de charger des scripts dans notre thème
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
+    // Chargement du style.css du thème parent BlankSlate 
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    // Chargement du theme.css du thème enfant Planty 
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/assets/css/theme.css'));
+}
+
+// Enregister un menu personnalisé
+
+add_action('init', 'register_my_menu');
+function register_my_menu()
+{
+    register_nav_menu('planty_menu', __('Menu Planty'));
+}
+
+
+// Hook pour l'affichage du menu Admin
+
+add_filter('wp_nav_menu_items', 'add_extra_item_to_nav_menu', 10, 2);
+function add_extra_item_to_nav_menu($items, $args)
+{
+    if (is_user_logged_in() && $args->theme_location == 'planty_menu') {
+        $items .= '<li class="admin_menu"><a href="#">Admin</a></li>';
+    }
+    return $items;
+}
+
+// Désactivation des styles Gutenberg
+
+add_action('wp_enqueue_scripts', function () {
+    wp_dequeue_style('wp-block-library');
+});
---
+++ wp-content/themes/planty_ok/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="description" content="<?php bloginfo('desc') ?>">
+    <meta name="viewport" content="width=device-width" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400;700;800&display=swap" rel="stylesheet">    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/assets/css/theme.css">
+    <?php wp_head(); ?>
+    <title><?php bloginfo('name') ?></title>
+</head>
+
+<body>
+
+<header>
+    <div class="logo">
+        <a href="#">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/logo.png" alt="Logo Planty">
+        </a>
+    </div>
+    <nav>
+        <?php
+           wp_nav_menu( array(
+            'menu'				=> "", // (int|string|WP_Term) Desired menu. Accepts a menu ID, slug, name, or object.
+            'menu_class'		=> "", // (string) CSS class to use for the ul element which forms the menu. Default 'menu'.
+            'menu_id'			=> "m_planty", // (string) The ID that is applied to the ul element which forms the menu. Default is the menu slug, incremented.
+            'container'			=> "",
+            'theme_location' => "planty_menu"
+        ) );
+        ?>
+    </nav>
+</header>
